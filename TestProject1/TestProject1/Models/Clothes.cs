bbcb53d91cdb2967446729ae9a7e4f1622dcb1c5
Vahedi <vahedirahele2@gmail.com>
2025-10-18 21:06:53 +0330

    Package Models classes

---
+++ TestProject1/TestProject1/Models/Clothes.cs
@@ -0,0 +1,11 @@
+﻿namespace TestProject1.Models;
+
+public class Clothes : Item
+{
+    public Clothes(int itemId, string name, decimal price, int size) : base(itemId, name, price)
+    {
+        this.size = size;
+    }
+
+    public int size { get; set; }
+}
---
+++ TestProject1/TestProject1/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace TestProject1.Models;
+
+public class User
+{
+    public User(string username, string password)
+    {
+        Username = username;
+        Password = password;
+    }
+    public User(string username, string password, string email, bool isAdmin)
+    {
+        Username = username;
+        Password = password;
+        Email = email;
+        IsAdmin = isAdmin;
+    }
+
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public bool IsAdmin { get; set; }
+}
