b732cb4acd0c3272f2e02a9e84e2a0014cec71cb
Vahedi <vahedirahele2@gmail.com>
2025-10-18 22:23:11 +0330

    Package Service classes

---
+++ second exersice/Service/IItemService.cs
@@ -0,0 +1,11 @@
+﻿using second_exersice;
+
+namespace Service;
+
+public interface IItemService
+{ 
+    public bool AddItem(Item item, User user);
+    public bool RemoveItem(int itemId, User user);
+    public bool PurchaseItem(int itemId, User user);
+    public List<Item> GetAllItems();
+}
---
+++ second exersice/Service/UserService.cs
@@ -0,0 +1,52 @@
+﻿using second_exersice;
+
+namespace Service;
+
+public class UserService
+{
+    private List<User> users = new List<User>();
+
+    public bool Register(User newUser)
+    {
+        if (users.Count == 0)
+        {
+            users.Add(newUser);
+            return true;
+        }
+        else
+        {
+            for (int i=0; i<users.Count; i++)
+            {
+                if (users[i].Username == newUser.Username)
+                    return false;
+            }
+            users.Add(newUser);
+            return true;
+        }
+    }
+
+    public User Login(string username, string password)
+    {
+        for (int i = 0; i < users.Count; i++)
+        {
+            if (users[i].Username == username && users[i].Password == password)
+            {
+                return users[i];
+            }
+        }
+        return null;
+    }
+
+    public bool UpdateUser(string username,string newEmail)
+    {
+        for (int i = 0; i < users.Count; i++)
+        {
+            if (users[i].Username == username)
+            {
+                users[i].Email = newEmail;
+                return true;
+            }
+        }
+        return false;
+    }
+}
