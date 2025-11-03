fbab367affac41990c4fdb494afba5088c3c0a81
Vahedi <vahedirahele2@gmail.com>
2025-11-03 15:36:05 +0330

    Domain Directory of MarketPlace Project

---
+++ nextExersice/WebApplication1/Marketplace.Domain/Item.cs
@@ -0,0 +1,17 @@
+﻿namespace Marketplace.Domain;
+
+public class Item
+{
+    public Item(int itemId, string name, decimal price)
+    {
+        ItemId = itemId;
+        Name = name;
+        Price = price;
+    }
+
+    public int ItemId { get; set; } 
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    
+    public ICollection<User> Users { get; set; } = new List<User>();
+}
---
+++ nextExersice/WebApplication1/Marketplace.Domain/User.cs
@@ -0,0 +1,30 @@
+﻿namespace Marketplace.Domain;
+
+public class User
+{
+    public int Id;
+    public User(string username, string password)
+    {
+        Username = username;
+        Password = password;
+    }
+    public User(string username, string password, string email, bool isAdmin)
+    {
+        Id++;
+        Username = username;
+        Password = password;
+        Email = email;
+        IsAdmin = isAdmin;
+    }
+
+    public int getId()
+    {
+        return Id;
+    }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public bool IsAdmin { get; set; }
+    
+    public ICollection<Item> Items { get; set; } = new List<Item>();
+}
